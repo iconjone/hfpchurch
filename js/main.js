b2efd10b09c12f935c5020134b06ce7a1d8f60dd
Jonathan Davis Samuel <jone.bala@gmail.com>
2019-05-22 22:38:58 -0500

    gallery

--- js/main.js
+++ js/main.js
@@ -90,6 +90,34 @@
 	};
 	carousel();
 
+  var gallery = function() {
+    $('.carousel-testimony').owlCarousel({
+      center: true,
+      loop: true,
+      items:1,
+      margin: 30,
+      stagePadding: 0,
+      nav: true,
+      autoplay: true,
+      stopOnHover: true,
+      rewindNav: true,
+      navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
+      responsive:{
+        0:{
+          items: 1
+        },
+        600:{
+          items: 2
+        },
+        1000:{
+          items: 3
+        }
+      }
+    });
+
+  };
+  gallery();
+
 	$('nav .dropdown').hover(function(){
 		var $this = $(this);
 		// 	 timer;

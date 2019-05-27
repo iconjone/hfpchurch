067e4c14a7620605de02ae3904bb762b1ffe3e92
Jonathan Davis Samuel <jone.bala@gmail.com>
2019-05-27 00:46:54 -0500

    Test of GUI for Events

---
+++ js/mail.js
@@ -0,0 +1,28 @@
+function ValidateEmail(mail) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+    return (true)
+  }
+  newsFormElement = document.getElementById('pNews')
+  newsFormElement.innerHTML = "Make sure it's a valid email!"
+  return (false)
+}
+
+function submitNews() {
+  if (ValidateEmail(document.getElementById('nEmailAddress').value)) {
+    url = "https://docs.google.com/forms/d/e/1FAIpQLSdJrt-E7YTTcA0i1iIuItkp7NMjuDjU1tiaaCkatvmztKh8xA/formResponse?&entry.2055715363="
+    text = document.getElementById('nEmailAddress').value
+    submit = '&submit=Submit'
+    //file_get_contents(url+text+submit);
+    fetch(url + text + submit, {
+      'mode': 'no-cors'
+    }).then(data => {
+      console.log(data)
+    })
+    newsFormElement = document.getElementById('newsForm')
+    newsFormElement.innerHTML = " "
+    newsFormElement = document.getElementById('pNews')
+    newsFormElement.innerHTML = " "
+    newsTextElement = document.getElementById('newsText')
+    newsTextElement.innerHTML = "Congrats! You've registered!"
+  }
+}
